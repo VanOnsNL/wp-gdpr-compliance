81dcff20e003f6bfcc898a128a5379cbf702292c
Michael van Tulder <michael@van-ons.nl>
2018-02-14 11:47:31 +0100

    Added uninstall handle

--- wp-gdpr-compliance/uninstall.php
+++ wp-gdpr-compliance/uninstall.php
@@ -4,3 +4,11 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die();
 }
+
+global $wpdb;
+
+$options = $wpdb->get_results( "SELECT option_name FROM ".$wpdb->options." WHERE option_name LIKE 'wpgdprc_%'");
+
+foreach( $options as $option ) {
+    delete_option( $option->option_name );
+}
